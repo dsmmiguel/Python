e879d962c58a48cc9ac896f0061b7b6b317175c4
dsmmiguel <dosantosmendesmiguel@gmail.com>
2025-12-15 10:06:23 -0300

    Add files via upload

---
+++ 2025.12.15 - teste numeros gov.py
@@ -0,0 +1,26 @@
+#2025.12.15 - teste numeros gob.py
+
+numero = input("Digite um número de 0 a 9: ")
+
+if numero == "0":
+    print("Você digitou zero")
+elif numero == "1":
+    print("Você digitou um")
+elif numero == "2":
+    print("Você digitou dois")
+elif numero == "3":
+    print("Você digitou três")
+elif numero == "4":
+    print("Você digitou quatro")
+elif numero == "5":
+    print("Você digitou cinco")
+elif numero == "6":
+    print("Você digitou seis")
+elif numero == "7":
+    print("Você digitou sete")
+elif numero == "8":
+    print("Você digitou oito")
+elif numero == "9":
+    print("Você digitou nove")
+else:
+    print("Número inválido")
